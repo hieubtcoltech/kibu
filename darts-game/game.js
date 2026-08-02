da0c8a70fda46347c45e7ac85f6b606865dbbfb9
Hieu Bui <hieubtcoltech@gmail.com>
2026-08-02 12:41:45 +0700

    Missed

--- darts-game/game.js
+++ darts-game/game.js
@@ -907,7 +907,7 @@
                 // Mũi tiêu vừa rồi không trúng quả nào -> đứt chuỗi
                 this.streak = 0;
                 this.onFire = false;
-                this.addFx(T('Trượt!', 'Missed!'), '#ffb3b3', this.cx, THROW_Y - 130, 20);
+                this.addFx('Missed', '#ffb3b3', this.cx, THROW_Y - 130, 20);
                 Sfx.miss();
             }
             this.pendingHits = 0;
